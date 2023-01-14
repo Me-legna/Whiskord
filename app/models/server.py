2cd3668de97b003dd3de8c84c60b935cfe7d7979
Me-legna <111507103+Me-legna@users.noreply.github.com>
2023-01-14 15:54:39 -0500

    Update Server Model and made copy for "base" syntax

--- app/models/server.py
+++ app/models/server.py
@@ -1,10 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.schema import Column, ForeignKey
-# from sqlalchemy.types import Integer, String, Float, Boolean, DateTime, Enum
-# from sqlalchemy.orm import relationship, validates
-# from .channels import Channel
-# from .user import User
+
 
 server_members = db.Table(
     "server_members",
@@ -22,9 +17,42 @@ class Server(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     ownerId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    image_url = db.Column(db.String)
     is_private = db.Column(db.Boolean, default=False, nullable=False)
     is_dm = db.Column(db.Boolean, default=False, nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
 
     members = db.relationship('User', secondary=server_members, backpopulates='servers')
     channels = db.relationship('Channel', backpopulates='server')
+
+
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.schema import Column, ForeignKey, MetaData, Table
+# from sqlalchemy.types import Integer, String, Float, Boolean
+# from sqlalchemy.orm import relationship, validates
+
+# base = declarative_base()
+
+# server_members = Table(
+#     "server_members",
+#     MetaData,
+#     Column("users", Integer, ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
+#     Column("servers", Integer, ForeignKey(add_prefix_for_prod('servers.id')), primary_key=True)
+# )
+
+# class Server(base):
+#     __tablename__ = 'servers'
+
+#     if environment == "production":
+#         __table_args__ = {'schema': SCHEMA}
+
+#     id = Column(db.Integer, primary_key=True)
+#     name = Column(String(100), nullable=False)
+#     ownerId = Column(Integer, ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+#     image_url = Column(String)
+#     is_private = Column(Boolean, default=False, nullable=False)
+#     is_dm = Column(Boolean, default=False, nullable=False)
+#     capacity = Column(Integer, nullable=False)
+
+#     members = relationship('User', secondary=server_members, backpopulates='servers')
+#     channels = relationship('Channel', backpopulates='server')
