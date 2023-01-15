9b13175efb18d9e37d02ba99958c5f258e11fc3a
Andrew Bierman <abbierman101@gmail.com>
2023-01-15 01:12:44 -0500

    running migrations with alembic

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -20,6 +20,9 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_servers()
+        undo_channels()
+        undo_messages()
     seed_users()
     # Add other seed functions here
     seed_servers()
