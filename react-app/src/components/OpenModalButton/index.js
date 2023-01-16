7a8520f5f3b195899f6c719a8df43b0aafc6d204
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-01-16 16:25:35 -0500

    single server component completed

---
+++ react-app/src/components/OpenModalButton/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useModal } from '../../context/Modal';
+
+function OpenModalButton({
+  modalComponent, // component to render inside the modal
+  buttonText, // text of the button that opens the modal
+  onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
+  onModalClose // optional: callback function that will be called once the modal is closed
+}) {
+  const { setModalContent, setOnModalClose } = useModal();
+
+  const onClick = () => {
+    if (typeof onButtonClick === 'function') onButtonClick();
+    if (typeof onModalClose === 'function') setOnModalClose(onModalClose);
+    setModalContent(modalComponent);
+  };
+
+  return (
+    <div>
+      <button className='button new-button' onClick={onClick}>{buttonText}</button>
+    </div>
+  );
+}
+
+export default OpenModalButton;
