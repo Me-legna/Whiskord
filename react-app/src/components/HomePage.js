ab55923dd7cb8c29ed8bc021906846ea9b9e53bc
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-01-15 19:18:35 -0500

    home page component skeleton

---
+++ react-app/src/components/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../../index.css';
+// import homepage.css;
+
+
+function HomePage() {
+   return (
+
+    <div className='home-page-main-div'>
+        <div className='home-page-header'>
+            <NavLink exact to="/">
+                <img className='logo' ></img>
+            </NavLink>
+            <NavLink className="undeveloped-feat" exact to="feature under review page">Download</NavLink>
+            <NavLink className="undeveloped-feat" exact to="feature under review page">Nitro</NavLink>
+            <NavLink className="undeveloped-feat" exact to="feature under review page">Discover</NavLink>
+            <NavLink className="undeveloped-feat" exact to="feature under review page">Contact Support</NavLink>
+            <NavLink className="login-redirect" exact to="login-page">Login</NavLink>
+        </div>
+
+    </div>
+   )
+}
