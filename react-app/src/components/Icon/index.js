6191b3202b599f0e05fd2f3f6bf0ab5a2f4a5303
Me-legna <111507103+Me-legna@users.noreply.github.com>
2023-01-17 17:07:59 -0500

    Reusable Icon Component

--- react-app/src/components/Icon/index.js
+++ react-app/src/components/Icon/index.js
@@ -1,10 +1,18 @@
+import './Icon.css'
+
 //set up to be resuable component to use for serverIcon, privateServerIcon userMessageIcon, memberIcon, createServerIcon
+//example:
+//<Icon isServer={true} clickEvent={() => console.log('hello')} imageUrl='https://i.pinimg.com/736x/3e/bd/e7/3ebde7a4c66db4c5c6f3ead8f8fa3205.jpg' />
+
 function Icon({
-    iconImage, //image to render
-    isServer=false //give css class for hover effect if True
+    imageUrl, //image to render
+    isServer=false, //give css class for hover effect if True
+    clickEvent=null //pass a custom clickevent for Icon
 }){
     return (
-        <></>
+        <div className={`icon-img ${isServer ? 'server-icon' : ''}`} onClick={clickEvent ? clickEvent : ()=> null}>
+            <img src={imageUrl} alt='icon' className={`icon-img ${isServer ? 'server-icon' : ''}`} />
+        </div>
     )
 }
 
