97c2f61480453f60991a6b2d27729b9f552a8a13
sohinib12 <sohini.bonthala@gmail.com>
2023-01-17 23:46:03 -0800

    added 404 error animation

--- react-app/src/components/404Page/index.js
+++ react-app/src/components/404Page/index.js
@@ -1,14 +1,36 @@
-import { Link } from 'react-router-dom'
-import './404.css'
+import { Link } from "react-router-dom";
+import "./404.css";
 
-function NotFoundPage () {
+function NotFoundPage() {
+  return (
+    // <div className='404'>
+    //     <h1>404 Page Not Found 😥</h1>
+    //     <Link to='/'>I wanna go home!</Link>
+    // </div>
+    <div class="page_404">
+      <div class="container">
+        <div class="row">
+          <div class="text-center">
+            <div class="animation_bg">
+              <h1 class="error-title text-center ">404</h1>
+            </div>
 
-    return (
-        <div className='404'>
-            <h1>404 Page Not Found 😥</h1>
-            <Link to='/'>I wanna go home!</Link>
+            <div class="message_box_404">
+              <h3 class="h2">We're sorry!</h3>
+
+              <p>
+                Looks like he was trying to figure out the wires! We're on it,
+                hang tight!
+              </p>
+              <Link to="/" class="link_404">
+                Go to Home
+              </Link>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
