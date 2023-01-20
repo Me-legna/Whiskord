495edc1c5af5766b0225ce439962e31e204c3df2
Andrew Bierman <abbierman101@gmail.com>
2023-01-20 14:56:19 -0500

    commented out some logs, added some links to homepage

--- react-app/src/components/LandingPage/SplashBanner.js
+++ react-app/src/components/LandingPage/SplashBanner.js
@@ -1,13 +1,16 @@
 import splash_section_primary_image_1 from '../../images/LandingPage/splash-section-primary-image-1.svg';
 import splash_section_image_2 from '../../images/LandingPage/splash-section-image-2.svg';
+import { useHistory } from 'react-router-dom';
 
 
 function SplashBanner() {
+    const history = useHistory();
+
     return (
         <div className="splash-banner">
             <h1>IMAGINE A PLACE...</h1>
             <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
-            <button>Open Whiskord in your browser</button>
+            <button onClick={() => history.push('/home')}>Open Whiskord in your browser</button>
             <div className='splash-images-container'>
                 <img src={splash_section_primary_image_1} alt='Splash Image' />
                 <img src={splash_section_image_2} alt='Splash Image 2' />
