3df1774c04172a5ef4ecb4ffc4a988de8b70dd9d
sohinib12 <sohini.bonthala@gmail.com>
2023-01-20 09:42:16 -0800

    users seed data updated

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -9,6 +9,20 @@ def seed_users():
         username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
         username='bobbie', email='bobbie@aa.io', password='password')
+    demo = User(
+        username='Demo', email='demo@aa.io', password='password')
+    marnie = User(
+        username='marnie', email='marnie@aa.io', password='password')
+    bobbie = User(
+        username='bobbie', email='bobbie@aa.io', password='password')
+    demo = User(
+        username='Demo', email='demo@aa.io', password='password')
+    marnie = User(
+        username='marnie', email='marnie@aa.io', password='password')
+    bobbie = User(
+        username='bobbie', email='bobbie@aa.io', password='password')
+    bobbie = User(
+        username='bobbie', email='bobbie@aa.io', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
@@ -27,5 +41,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM users")
-        
+
     db.session.commit()
