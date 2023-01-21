dde648202d031b05719f5f909928909d4d6be7bd
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-01-21 17:09:42 -0500

    css for login and signup page

--- react-app/src/components/auth/index.js
+++ react-app/src/components/auth/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './LoginSignupPage.css';
 
@@ -8,13 +9,13 @@ export default function LoginSignUpPage() {
 
     return (
         <div className='main-container'>
-            {/* <div className='welcome'> */}
-                <h1>Welcome back!</h1>
-                <p>We're so excited to see you again!</p>
-                <div className='sign-up-container'>
+            <div className='welcome'>
+                <h1 style={{marginLeft:'160px'}}>Welcome back!</h1>
+                <p style={{margin:'0px 125px 0px 125px'}}>We're so excited to see you again!</p>
+                <div className='sign-up-container' style={{padding: '30px 100px 0px 0px'}}>
                     <LoginForm />
                 </div>
-            {/* </div> */}
+            </div>
             <div className='qr-code-container'>
                 <h2>Whiskord Repo</h2>
                 <img src={qrCode} alt='qr-code' />
