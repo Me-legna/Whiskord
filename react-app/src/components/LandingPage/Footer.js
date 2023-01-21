f40982f1157124a691f3ef20275cf36e81865cb8
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-01-21 00:25:59 -0500

    footer github icons added

--- react-app/src/components/LandingPage/Footer.js
+++ react-app/src/components/LandingPage/Footer.js
@@ -3,10 +3,10 @@ export default function Footer(){
     return (
         <div className='footer'>
             <div className='footer-links'>
-                <a href='https://github.com/andrew-bierman'>BDAWG</a>
-                <a href='https://github.com/Me-legna'>IN THE ARMS OF AN ANGEL</a>
-                <a href='https://github.com/andrea-green'>PIGGYSMOLS</a>
-                <a href='https://github.com/sohinib12'>SOE</a>
+                <a href='https://github.com/andrew-bierman'>Andrew Bierman</a>
+                <a href='https://github.com/Me-legna'>Angel Gonzalez</a>
+                <a href='https://github.com/andrea-green'>Andrea Green</a>
+                <a href='https://github.com/sohinib12'>Sohini Bonthala</a>
             </div>
 
         </div>
