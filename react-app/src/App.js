b999003582bc98ffc98fc5938a34d465eb1e5b6a
sohinib12 <sohini.bonthala@gmail.com>
2023-01-21 22:50:07 -0800

    fixed bugs for the forms

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,216 +1,199 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { authenticate } from './store/session';
-import LogoutButton from '../src/components/auth/LogoutButton'
-import Routing from './Routing';
-import HomePage from './components/HomePage';
-import NavBar from './components/LandingPage/Navigation/NavBar';
-import LandingPage from './components/LandingPage';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import Chat from './components/Chat/Chat';
-import { Route, Switch, useHistory } from 'react-router-dom';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
-import UsersList from './components/Users/UsersList';
-import User from './components/Users/User';
-import NotFoundPage from './components/404Page';
-import PrivateServers from './components/HomePage/Body/ServerComps/PrivateServers';
-import ServerList from './components/HomePage/Body/ServerComps/ServerList';
-import AllChannels from './components/HomePage/Body/Channels/AllChannels';
-import PrivateBody from './components/HomePage/Body/PrivateBodyRight';
-import PublicBody from './components/HomePage/Body/PublicBodyRight';
-import Icon from './components/Icon';
-import { addServer, privateServers, publicServers, resetServerDetails } from './store/server';
-import IconModal from './components/Icon/IconModal';
-import CreatePrivateServerForm from './components/HomePage/Body/ServerComps/CreatePrivateServerForm';
-import OpenModalButton from './components/OpenModalButton';
-import CreatePublicServerForm from './components/HomePage/Body/ServerComps/CreatePublicServerForm';
-import { resetMessageState } from './store/message';
-import { resetChannelState } from './store/channel';
-import LoginSignUpPage
-    from './components/auth';
-import UnderDevelopment from './components/UnderDevelopment';
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { authenticate } from "./store/session";
+import LogoutButton from "../src/components/auth/LogoutButton";
+import Routing from "./Routing";
+import HomePage from "./components/HomePage";
+import NavBar from "./components/LandingPage/Navigation/NavBar";
+import LandingPage from "./components/LandingPage";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Chat from "./components/Chat/Chat";
+import { Route, Switch, useHistory } from "react-router-dom";
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
+import UsersList from "./components/Users/UsersList";
+import User from "./components/Users/User";
+import NotFoundPage from "./components/404Page";
+import PrivateServers from "./components/HomePage/Body/ServerComps/PrivateServers";
+import ServerList from "./components/HomePage/Body/ServerComps/ServerList";
+import AllChannels from "./components/HomePage/Body/Channels/AllChannels";
+import PrivateBody from "./components/HomePage/Body/PrivateBodyRight";
+import PublicBody from "./components/HomePage/Body/PublicBodyRight";
+import Icon from "./components/Icon";
+import {
+  addServer,
+  privateServers,
+  publicServers,
+  resetServerDetails,
+} from "./store/server";
+import IconModal from "./components/Icon/IconModal";
+import CreatePrivateServerForm from "./components/HomePage/Body/ServerComps/CreatePrivateServerForm";
+import OpenModalButton from "./components/OpenModalButton";
+import CreatePublicServerForm from "./components/HomePage/Body/ServerComps/CreatePublicServerForm";
+import { resetMessageState } from "./store/message";
+import { resetChannelState } from "./store/channel";
+import LoginSignUpPage from "./components/auth";
+import UnderDevelopment from "./components/UnderDevelopment";
+import "./App.css";
 // import WhiskordLogoCrop from './images/WhiskordLogoCrop.png'
-import WhiskordLogo from './images/Logo/WhiskordLogoCrop.png';
-
-
+import WhiskordLogo from "./images/Logo/WhiskordLogoCrop.png";
 
 function App() {
-    const dispatch = useDispatch();
-    const history = useHistory()
-    const [loaded, setLoaded] = useState(false);
-    const user = useSelector(state => state.session.user)
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [loaded, setLoaded] = useState(false);
+  const user = useSelector((state) => state.session.user);
 
-    const singleServer = useSelector((state) => state?.servers?.singleServer);
+  const singleServer = useSelector((state) => state?.servers?.singleServer);
 
-    const allChannels = useSelector((state) => state?.servers?.singleServer?.Channels);
+  const allChannels = useSelector(
+    (state) => state?.servers?.singleServer?.Channels
+  );
 
-    // const allChannels = useSelector((state) => state?.channels?.allChannels);
+  // const allChannels = useSelector((state) => state?.channels?.allChannels);
 
-    useEffect(() => {
-        dispatch(authenticate()).then(() => setLoaded(true));
-        if (user) {
-            dispatch(publicServers());
-        }
-    }, [dispatch]);
-    // useEffect(() => {
-    //     (async () => {
-    //         await dispatch(authenticate());
-    //         setLoaded(true);
-    //     })();
-    // }, [dispatch]);
-
-    const getPrivateServers = () => {
-        dispatch(resetMessageState())
-        dispatch(resetChannelState())
-        dispatch(resetServerDetails())
-        dispatch(privateServers())
-        history.push('/home/@me')
+  useEffect(() => {
+    dispatch(authenticate()).then(() => setLoaded(true));
+    if (user) {
+      dispatch(publicServers());
     }
+  }, [dispatch]);
+  // useEffect(() => {
+  //     (async () => {
+  //         await dispatch(authenticate());
+  //         setLoaded(true);
+  //     })();
+  // }, [dispatch]);
 
+  const getPrivateServers = () => {
+    dispatch(resetMessageState());
+    dispatch(resetChannelState());
+    dispatch(resetServerDetails());
+    dispatch(privateServers());
+    history.push("/home/@me");
+  };
 
-    if (!loaded) {
-        return null;
-    }
+  if (!loaded) {
+    return null;
+  }
 
-    return (
-        <div className='entire-homepage-div'>
-            {!user
-                ? (
-                    <>
-                        {/* <NavBar /> */}
-                        <Switch>
-                            < Route path="/" exact={true}>
-                                <LandingPage />
-                            </Route>
-                            <Route path="/login" exact={true}>
-                                <LoginSignUpPage />
-                            </Route>
-                            <Route path="/sign-up" exact={true}>
-                                <SignUpForm />
-                            </Route>
-                            <ProtectedRoute path="/users" exact={true}>
-                                <UsersList />
-                            </ProtectedRoute>
-                            <ProtectedRoute path="/users/:userId" exact={true}>
-                                <User />
-                            </ProtectedRoute>
-                            <Route path='/404'>
-                                <NotFoundPage />
-                            </Route>
-                            <Route path='/next'>
-                                < UnderDevelopment />
-                            </Route>
-                        </Switch>
-                    </>
-                )
-                : (
-                    <Switch>
-                        {/* Landing Page */}
-                        < Route path="/" exact={true}>
-                            <LandingPage />
-                        </Route>
-                        <ProtectedRoute path='/home'>
+  return (
+    <div className="entire-homepage-div">
+      {!user ? (
+        <>
+          {/* <NavBar /> */}
+          <Switch>
+            <Route path="/" exact={true}>
+              <LandingPage />
+            </Route>
+            <Route path="/login" exact={true}>
+              <LoginSignUpPage />
+            </Route>
+            <Route path="/sign-up" exact={true}>
+              <SignUpForm />
+            </Route>
+            <ProtectedRoute path="/users" exact={true}>
+              <UsersList />
+            </ProtectedRoute>
+            <ProtectedRoute path="/users/:userId" exact={true}>
+              <User />
+            </ProtectedRoute>
+            <Route path="/404">
+              <NotFoundPage />
+            </Route>
+            <Route path="/next">
+              <UnderDevelopment />
+            </Route>
+          </Switch>
+        </>
+      ) : (
+        <Switch>
+          {/* Landing Page */}
+          <Route path="/" exact={true}>
+            <LandingPage />
+          </Route>
+          <ProtectedRoute path="/home">
+            <div className="app-container main-body">
+              <div className="server-list-container sidebar">
+                <div className="server-list-button" id="logo-button">
+                  <button className="" onClick={getPrivateServers}>
+                    {/* <img src={WhiskordLogo} /> */}
+                    {/* Pr */}
+                  </button>
+                </div>
 
-                            <div className='app-container main-body'>
+                {/* Load all public servers */}
+                <ServerList />
 
-                                <div className='server-list-container sidebar'>
+                <div className="server-list-button">
+                  <OpenModalButton
+                    buttonText={"+"}
+                    modalComponent={<CreatePublicServerForm />}
+                  />
+                </div>
+              </div>
 
-                                    <div className="server-list-button" id='logo-button'>
-                                        <button className="" onClick={getPrivateServers}>
-                                            {/* <img src={WhiskordLogo} /> */}
-                                            {/* Pr */}
-                                        </button>
-                                    </div>
+              <div className="main-body">
+                <div className="channel-list-container left">
+                  <div className="server-name-header">
+                    {/* servername header component here*/}
+                    <h3>{singleServer && singleServer.name}</h3>
+                    {user && user.id === singleServer.owner_id && (
+                      <i className="fa-solid fa-gear"></i>
+                    )}
+                  </div>
 
+                  <div className="app-channel-list-container">
+                    {/* Route to load private server channel list */}
+                    <ProtectedRoute path="/home/@me">
+                      <PrivateServers />
+                    </ProtectedRoute>
+                    {/* Route to load public server channels list */}
+                    <ProtectedRoute path="/home/:serverId">
+                      {singleServer && !singleServer.is_private && (
+                        <AllChannels channels={allChannels} />
+                      )}
+                    </ProtectedRoute>
+                  </div>
 
-                                    {/* Load all public servers */}
-                                    <ServerList />
+                  <div className="app-user-and-logout-button">
+                    <div className="user-details">
+                      <i className="fa-solid fa-circle-user fa-xl"></i>
+                      &nbsp; &nbsp;
+                      {user.username}
+                    </div>
+                    <LogoutButton />
+                  </div>
+                </div>
 
+                <div className="middle-right-body">
+                  {/* Load messages, header, and members of Private Server */}
+                  <ProtectedRoute path="/home/@me/:channelId">
+                    <PrivateBody />
+                  </ProtectedRoute>
 
-                                    <div className="server-list-button">
-                                        <OpenModalButton
-                                            buttonText={'+'}
-                                            modalComponent={<CreatePublicServerForm />} />
-                                    </div>
+                  {/* Load messages, header, and members of Public Server */}
+                  <ProtectedRoute path="/home/:serverId/:channelId">
+                    <PublicBody />
+                  </ProtectedRoute>
+                </div>
+              </div>
+            </div>
+          </ProtectedRoute>
 
-                                </div>
-
-                                <div className='main-body'>
-                                    <div className='channel-list-container left'>
-                                        <div className='server-name-header'>
-
-                                            {/* servername header component here*/}
-                                            <h3>{singleServer && singleServer.name}</h3>
-                                            {
-                                                user && user.id === singleServer.owner_id &&
-
-                                                <i className="fa-solid fa-gear"></i>
-
-                                            }
-                                        </div>
-
-
-                                        <div className='app-channel-list-container'>
-
-                                            {/* Route to load private server channel list */}
-                                            <ProtectedRoute path="/home/@me" >
-                                                <PrivateServers />
-                                            </ProtectedRoute>
-                                            {/* Route to load public server channels list */}
-                                            <ProtectedRoute path="/home/:serverId" >
-                                                {(singleServer && !singleServer.is_private) &&
-                                                    <AllChannels channels={allChannels} />
-                                                }
-                                            </ProtectedRoute>
-                                        </div>
-
-                                        <div className='app-user-and-logout-button'>
-                                            <div className='user-details'>
-                                                <i className="fa-solid fa-circle-user fa-xl"></i>
-                                                &nbsp;
-                                                &nbsp;
-                                                {user.username}
-                                            </div>
-                                            <LogoutButton />
-                                        </div>
-
-                                    </div>
-
-
-                                    <div className='middle-right-body'>
-
-
-                                        {/* Load messages, header, and members of Private Server */}
-                                        <ProtectedRoute path="/home/@me/:channelId" >
-                                            <PrivateBody />
-                                        </ProtectedRoute>
-
-                                        {/* Load messages, header, and members of Public Server */}
-                                        <ProtectedRoute path="/home/:serverId/:channelId" >
-                                            <PublicBody />
-                                        </ProtectedRoute>
-                                    </div>
-                                </div>
-                            </div>
-                        </ProtectedRoute>
-
-                        {/* Error Page */}
-                        <Route path='/404'>
-                            <NotFoundPage />
-                        </Route>
-                        <LogoutButton />
-                        {/* UnderDevelopment Page */}
-                        <Route path='/next'>
-                            < UnderDevelopment />
-                        </Route>
-                    </Switch>
-                )
-            }
-
-        </div >
-    );
+          {/* Error Page */}
+          <Route path="/404">
+            <NotFoundPage />
+          </Route>
+          <LogoutButton />
+          {/* UnderDevelopment Page */}
+          <Route path="/next">
+            <UnderDevelopment />
+          </Route>
+        </Switch>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/HomePage/Body/ServerComps/PrivateServers.js
+++ react-app/src/components/HomePage/Body/ServerComps/PrivateServers.js
@@ -1,94 +1,123 @@
 //render list of servers where is_private == true
 import React, { useEffect } from "react";
-import { privateServers, publicServers, resetServerDetails, serverDetails } from "../../../../store/server";
+import {
+  privateServers,
+  publicServers,
+  resetServerDetails,
+  serverDetails,
+} from "../../../../store/server";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory } from "react-router-dom";
-import Icon from "../../../Icon"
+import Icon from "../../../Icon";
 import { resetMessageState } from "../../../../store/message";
 import { resetChannelState, getChannels } from "../../../../store/channel";
 import OpenModalButton from "../../../OpenModalButton";
 import CreatePrivateServerForm from "./CreatePrivateServerForm";
 import EditServerForm from "./EditServerForm";
 import DeleteServerForm from "./DeleteServerForm";
-
-
+import { useModal } from "../../../../context/Modal";
 
 function PrivateServers() {
-    const serversObj = useSelector((state) => state.servers.allPrivateServers.byId);
-    const servers = Object.values(serversObj);
-    const singleServer = useSelector(state => state.servers.singleServer)
-    const channel = useSelector(state => state.channels.channelDetails)
-    const channelId = channel.id
+  const serversObj = useSelector(
+    (state) => state.servers.allPrivateServers.byId
+  );
+  const servers = Object.values(serversObj);
+  const singleServer = useSelector((state) => state.servers.singleServer);
+  const channel = useSelector((state) => state.channels.channelDetails);
+  const { setModalContent, setOnModalClose } = useModal();
 
-    const dispatch = useDispatch();
-    const history = useHistory()
+  const channelId = channel.id;
 
-    useEffect(() => {
-        (async () => {
-            await dispatch(privateServers())
-            await dispatch(serverDetails(servers[0]?.id))
-            await dispatch(getChannels(singleServer.id))
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-        })();
-        // if (servers) {
-        //     if(singleServer){
-        //         if (!channelId) {
-        //         }
-        //     }
-        // } else {
-        // }
-    }, [dispatch]);
+  useEffect(() => {
+    (async () => {
+      await dispatch(privateServers());
+      if(servers[0]) await dispatch(serverDetails(servers[0]?.id));
+      await dispatch(getChannels(singleServer.id));
+    })();
+    // if (servers) {
+    //     if(singleServer){
+    //         if (!channelId) {
+    //         }
+    //     }
+    // } else {
+    // }
+  }, [dispatch]);
 
-    const privateServerDetails = (serverId) => {
-        dispatch(resetMessageState())
-        dispatch(resetChannelState())
-        dispatch(resetServerDetails())
-        dispatch(getChannels(serverId))
-        dispatch(serverDetails(serverId))
-        // history.push('/home/me/')
-    }
-    if (channelId) history.push(`/home/@me/${channelId}`)
+  const privateServerDetails = (serverId) => {
+    dispatch(resetMessageState());
+    dispatch(resetChannelState());
+    dispatch(resetServerDetails());
+    dispatch(getChannels(serverId));
+    dispatch(serverDetails(serverId));
+    // history.push('/home/me/')
+  };
+  const handlePrivateServerModal = () => {
+    setModalContent(<CreatePrivateServerForm />);
+  };
 
-    return (
-        <div className="private-servers-list">
-            <div className="flex">
-                <div className="flex">
-                    <h6>
-                        Direct Messages
-                    </h6>
-                    <OpenModalButton
-                        faIcon={<i className="fa-solid fa-plus" />}
-                        modalComponent={<CreatePrivateServerForm />}
-                    />
-                </div>
-                {
-                    Object.keys(singleServer).length ?
-                        <div>
-                            <OpenModalButton
-                                faIcon={<i className="fa-solid fa-pen-to-square" />}
-                                modalComponent={<EditServerForm />}
-                            />
-                            <OpenModalButton
-                                faIcon={<i className="fa-solid fa-trash-can" />}
-                                modalComponent={<DeleteServerForm />}
-                            />
-                        </div>
-                        : <></>
-                }
-            </div>
-            {servers.map((server, idx) => {
-                return (
-                    <div className="server-list-button" key={server.id}>
-                        <NavLink to={`/home/@me/${server.id}`}>
-                            <button onClick={() => privateServerDetails(server.id)}>
-                                {server.name[0]}
-                            </button>
-                        </NavLink>
-                    </div>
-                )
-            })}
+  const handleEditServerModal = () => {
+    setModalContent(<EditServerForm />);
+  };
+  const handleDeleteServerModal = (singleServer) => {
+    setModalContent(<DeleteServerForm server={singleServer} />);
+  };
+
+  if (channelId) history.push(`/home/@me/${channelId}`);
+
+  return (
+    <div className="private-servers-list">
+      <div className="flex">
+        <div className="flex">
+          <h6>Direct Messages</h6>
+          <button onClick={() => handlePrivateServerModal()}>
+            <i className="fa-solid fa-plus" />
+          </button>
+          {/* <OpenModalButton
+            faIcon={<i className="fa-solid fa-plus" />}
+            modalComponent={
+              <CreatePrivateServerForm onClose={handlePrivateServerModal} />
+            }
+          /> */}
         </div>
-    )
+        {Object.keys(singleServer).length ? (
+          <div>
+            <button onClick={() => handleEditServerModal()}>
+              <i className="fa-solid fa-pen-to-square" />
+            </button>
+            <button onClick={() => handleDeleteServerModal(singleServer)}>
+              <i className="fa-solid fa-trash-can" />
+            </button>
+            {/* <OpenModalButton
+              faIcon={<i className="fa-solid fa-pen-to-square" />}
+              modalComponent={
+                <EditServerForm onClose={handleEditServerModal} />
+              }
+            />
+            <OpenModalButton
+              faIcon={<i className="fa-solid fa-trash-can" />}
+              modalComponent={<DeleteServerForm />}
+            /> */}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      {servers.map((server, idx) => {
+        return (
+          <div className="server-list-button" key={server.id}>
+            <NavLink to={`/home/@me/${server.id}`}>
+              <button onClick={() => privateServerDetails(server.id)}>
+                {server.name[0]}
+              </button>
+            </NavLink>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default PrivateServers
+export default PrivateServers;
